0eefe6e4fd88d83cd9b71aac6d67e3f6c726847d
Edoardo Sorgentone <sorgentonedoardo@gmail.com>
2025-11-04 20:13:37 +0100

    inserito corretto client Idd

--- src/components/Checkout.tsx
+++ src/components/Checkout.tsx
@@ -193,7 +193,7 @@ const CheckoutInner: React.FC<CheckoutProps> = ({ items, onClose }) => {
 // Wrapper con PayPalScriptProvider
 const Checkout: React.FC<CheckoutProps> = (props) => {
   // 👉 Inserisci qui il tuo client-id (sandbox o live, quello che vuoi usare)
-  const PAYPAL_CLIENT_ID = "AVH-fUI-4jTe1e4AXbguImmxkBxgtykyg9U5ZjuPusvNBzC8_9Ldo5ScUSL_paQblihqTMDLouhpJro6";
+  const PAYPAL_CLIENT_ID = "AVH-fUI-AXdjP5lUjnZKZ5qCBphInIzSk1-tf-TCAF_JYoN25OZBysN6DqtN04WeTie7sXxSOQ0cpqW1I1KvIHPM";
 
   return (
     <PayPalScriptProvider
